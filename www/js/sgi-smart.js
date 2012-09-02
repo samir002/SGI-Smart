a7e682021b5cd0b2a7939f456fc3aad1567d9d49
jsnoriegam <jsnoriegam@hotmail.com>
2012-09-01 22:06:09 -0500

    Vamos a ver si hace algo...

--- www/js/sgi-smart.js
+++ www/js/sgi-smart.js
@@ -1,11 +1,30 @@
-deviceready = function(){
-	navigator.notification.alert('Hello, i\'m hello worlding!!');
-	
-	captureSite = function(){
-		
-	};
+error = function(error){
+	alert('code: '    + error.code    + '\n' +
+	          'message: ' + error.message + '\n');
 }
 
-$(document).ready(function(){
+captureSuccess = function(mediaFiles){
+	var i, path, len;
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].fullPath;
+        $('#fotos').append(path + '\n');
+    }
+}
+
+var geoSuccess = function(position) {
+	position.coords.latitude;
+	position.coords.longitude;
+};
+
+deviceready = function(){
+	navigator.geolocation.getCurrentPosition(geoSuccess, error);
+	
+	$('#tomar-foto').bind('click', function(event, ui){
+		navigator.notification.alert('Iniciando captura de imágenes');
+		navigator.device.capture.captureImage(captureSuccess, error, {limit:4});
+	});
+}
+
+$(document).bind('pageinit', function(){
 	document.addEventListener('deviceready', deviceready, true);
 });
