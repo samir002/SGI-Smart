8ad26cedf9a359a6aa7228179859e4b2cd331c39
jsnoriegam <jsnoriegam@hotmail.com>
2012-09-02 22:52:50 -0500

    Mejoras visuales y prueba con el evento menú

--- www/js/sgi-smart.js
+++ www/js/sgi-smart.js
@@ -12,15 +12,21 @@ pictureSuccess = function(imagesrc){
 	$('<img />').attr('src', imagesrc).css('display','block').css('width','100%').appendTo(fotos);
 };
 
-var geoSuccess = function(position) {
+geoSuccess = function(position) {
 	$('#latitud').val(position.coords.latitude);
 	$('#longitud').val(position.coords.longitude);
 };
 
+showMenu = function(){
+	$.mobile.changePage($('#config'));
+}
+
 deviceready = function(){
+	document.addEventListener("menubutton", showMenu, false);
+	
 	navigator.geolocation.watchPosition(geoSuccess, errorLog, { maximumAge: 3000, timeout: 15000, enableHighAccuracy: true });
 	$('#tomar-foto').unbind('tap').bind('tap', function(event, ui){
-		navigator.camera.getPicture(pictureSuccess, error, { quality: 70, targetWidth: 1024, targetHeight: 1024, destinationType: Camera.DestinationType.FILE_URI });
+		navigator.camera.getPicture(pictureSuccess, error, { quality: 75, targetWidth: 1024, targetHeight: 1024, destinationType: Camera.DestinationType.FILE_URI });
 	});
 	$('#upload-info').unbind('tap').bind('tap', function(event, ui){
 		
@@ -28,6 +34,8 @@ deviceready = function(){
 };
 
 $(document).ready(function(){
+	$.support.cors = true;
+	$.mobile.allowCrossDomainPages = true;
 	$.mobile.page.prototype.options.addBackBtn = true;
 	document.addEventListener('deviceready', deviceready, false);
 });
